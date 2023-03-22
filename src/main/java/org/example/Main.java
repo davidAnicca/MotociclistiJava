110e97f8692e735fc60e0baba1158b74c4f5d212
anicca <meresan02david1006@gmail.com>
2023-03-22 12:04:55 +0200

    LOGS RUNNING

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.impl.Log4jContextFactory;
 import org.example.entity.*;
 import org.example.repo.*;
 
@@ -19,6 +20,7 @@ public class Main {
     public static void main(String[] args) {
 
         log.debug("test debug");
+
         Properties props = new Properties();
         try {
             log.error("test - this is not an error!");
